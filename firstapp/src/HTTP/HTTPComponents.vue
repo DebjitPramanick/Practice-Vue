c0faf8252df76108b443e26b3fa54881655f6cc6
DebjitPramanick <dpramanick9990@gmail.com>
2021-11-16 19:57:32 +0530

    Lifecycle methods

--- firstapp/src/HTTP/HTTPComponents.vue
+++ firstapp/src/HTTP/HTTPComponents.vue
@@ -3,6 +3,10 @@
         <h3>HTTP Component</h3>
         <button @click="getPosts()">Fetch Posts</button>
         <p v-for="p in posts.slice(0, 5)" :key="p.id">{{p.title}}</p>
+        <h3>GET Req using lifecycle method</h3>
+        <div class="flex">
+            <p v-for="p in lcPosts.slice(0, 5)" :key="p.id">{{p.title}}</p>
+        </div>
     </div>
 </template>
 
@@ -10,9 +14,14 @@
     import axios from "axios"
     export default {
         name: "HTTPComponent",
+        created(){
+            axios.get("https://jsonplaceholder.typicode.com/posts")
+                .then(res => this.lcPosts = res.data)
+        },
         data(){
             return{
-                posts: []
+                posts: [],
+                lcPosts: []
             }
         },
         methods: {
