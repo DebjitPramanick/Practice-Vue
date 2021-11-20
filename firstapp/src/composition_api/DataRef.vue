77c706a27eb39c5562e602941f893f21dbbecf34
DebjitPramanick <dpramanick9990@gmail.com>
2021-11-20 19:30:45 +0530

    Reactive in place of refs

--- firstapp/src/composition_api/DataRef.vue
+++ firstapp/src/composition_api/DataRef.vue
@@ -2,18 +2,26 @@
     <div class="container">
         <h3>Composition API</h3>
         <p>Composition val - {{cFirstName}}</p>
+        <p>Reactive vals - {{state.name}}, {{state.age}}, {{state.occupation}}</p>
     </div>
 </template>
 
 <script>
-    import {ref} from 'vue'
+    import {ref, reactive} from 'vue'
     export default {
         name: "DataRef",
         setup(){
             const cFirstName = ref('Clark') // Reactive reference to the value (Reactive and Mutable)
 
+            const state = reactive({ // Same as ref but takes everything at once
+                name: 'Debjit',
+                age: 21,
+                occupation: 'Developer'
+            })
+
             return {
-                cFirstName
+                cFirstName,
+                state
             }
         }
     }
